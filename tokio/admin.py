290f8c51034030e6fc84b7c850f8efb2177098dc
Sebastian <js.millan10@uniandes.edu.co>
2020-02-17 01:09:06 -0500

    Permitiendo entrar al admin desde heroku

--- tokio/admin.py
+++ tokio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+# Puedes acceder al admin con el usuario admin y contraseña admin
 admin.site.register(Deportistas)
 admin.site.register(Deportes)
 admin.site.register(Entrenadores)
